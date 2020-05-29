1bb36594b6e762df829518c7145cd4af117767e6
Sambalim <limsungho07@hanmail.net>
2020-05-29 23:30:07 +0900

    19. Header & Footer Components

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<header>
+  <h3>
+    <img src="/img/logo.svg" alt="Poll Sambalim Logo">
+  </h3>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+  h3 {
+    margin: 0;
+    text-align: center;
+  }
+  img { 
+    max-width: 100px;
+  }
+</style>
