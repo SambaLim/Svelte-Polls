13614338118ffef669013029b96424c16a1997a2
Sambalim <limsungho07@hanmail.net>
2020-06-28 00:29:56 +0900

    35. Wrap up

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -1,14 +1,6 @@
 import { writable } from 'svelte/store';
 
 const PollStore = writable([
-  {
-    id: 1,
-    question: 'Javascript or Python',
-    answerA: 'Javascript',
-    answerB: 'Python',
-    votesA: 15,
-    votesB: 9
-  }
 ]);
 
 export default PollStore;
